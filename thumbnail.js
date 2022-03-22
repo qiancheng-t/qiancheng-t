e7586d62a00835ce6851eb7b53050ccf1a88f4d1
qiancheng(千城君) <992887291@qq.com>
2022-03-22 23:36:21 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function () {
-        alert("点击了图片");
-        targetImage.classList.remove("small");
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        } else {
+            targetImage.classList.add("small");
+        }
     });
 });
